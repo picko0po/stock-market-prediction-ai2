5120b74bec8047fa1fa21f7298011c2682eca732
picko0po <intelligentpick590@gmail.com>
2026-09-03 23:15:33 +0300

    Update dataset.py

--- dataset.py
+++ dataset.py
@@ -3,12 +3,13 @@ from torch.utils.data import Dataset
 
 
 class LSTMStocksDataset(Dataset):
-    def __init__(self, x_tensor: torch.Tensor, y_tensor: torch.Tensor):
-        self._x_tensor = x_tensor
-        self._y_tensor = y_tensor
 
-    def __len__(self):
-        return self._y_tensor.shape[0]
+    def init(self, x_tensor, y_tensor):
+        self._x_tensor = x_tensor.float()
+        self._y_tensor = y_tensor.float()
 
-    def __getitem__(self, idx: int):
-        return self._x_tensor[idx, :], self._y_tensor[idx]
+    def len(self):
+        return len(self._y_tensor)
+
+    def getitem(self, idx):
+        return  self._x_tensor[idx],  self._y_tensor[idx]
